8ca1b4f122b64225d1a0ec909c1fd3f121e8d909
hmed <ahmedsahbaoui561@gmail.com>
2024-02-10 13:58:13 +0100

    First Commit

---
+++ .config/hypr/UserScripts/RofiBeats.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Directory for icons
+iDIR="$HOME/.config/swaync/icons"
+
+# Note: You can add more options below with the following format:
+# ["TITLE"]="link"
+
+# Define menu options as an associative array
+declare -A menu_options=(
+  ["Lofi Girl ☕️🎶"]="https://play.streamafrica.net/lofiradio"
+  ["Jojer Nectard 🐝🎶"]="https://youtube.com/playlist?list=PLhrG05fbuPKdFR8dBbGPa2k3H3y3rspPS"
+  ["Chloe Burbank 🎶"]="https://youtube.com/playlist?list=PLuiczBQhAY_iznTpd9QjNGwWa5e2vov7o"
+)
+
+# Function for displaying notifications
+notification() {
+  notify-send -u normal -i "$iDIR/music.png" "Playing now: $@"
+}
+
+# Main function
+main() {
+  choice=$(printf "%s\n" "${!menu_options[@]}" | rofi -dmenu -i -p "")
+
+  if [ -z "$choice" ]; then
+    exit 1
+  fi
+
+  link="${menu_options[$choice]}"
+
+  notification "$choice"
+  
+  # Check if the link is a playlist
+  if [[ $link == *playlist* ]]; then
+    mpv --vid=no "$link"
+  else
+    mpv --vid=no "$link"
+  fi
+}
+
+# Check if an online music process is running and send a notification, otherwise run the main function
+pkill mpv && notify-send -u low -i "$iDIR/music.png" "Online Music stopped" || main

edabc7f703e668d5787a3a99d3f9e376d0ee0c2c
AhmedSahbaoui69 <ahmedsahbaoui561@gmail.com>
2024-11-09 16:03:55 +0100

    Remove unused scripts and update waybar config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,9 +9,11 @@ alias pls "sudo"
 alias yeet "yay -Rns"
 alias adios "shutdown -h now" 
 alias ls "eza"
+alias cat "bat"
 
 set -Ux PYENV_ROOT $HOME/.pyenv
 fish_add_path $PYENV_ROOT/bin
 pyenv init - | source
 
 set -gx BROWSER /usr/bin/zen-browser
+set -gx CRYPTOGRAPHY_OPENSSL_NO_LEGACY true

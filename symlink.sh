beffbeb0a7951e945c5d09caf764cde3e624c348
AhmedSahbaoui69 <ahmedsahbaoui561@gmail.com>
2025-12-08 10:51:43 +0100

    Refactor symlink script to use absolute paths for dotfiles and destinations

--- symlink.sh
+++ symlink.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env fish
 
-set dotdir ~/dotfiles
+set dotdir (realpath ~/dotfiles)
 set symfile $dotdir/sym_paths.txt
 
 for path in (cat $symfile)
     # Absolute path inside ~/dotfiles
-    set src (realpath $path)
-    # Remove ~/dotfiles/ prefix
-    set rel (string replace -r "^$dotdir/" "" $src)
+    set src $dotdir/$path
     # Destination in ~/
-    set dest ~/$rel
+    set dest (realpath ~)/$path
     # Ensure parent directory exists
     mkdir -p (dirname $dest)
     # Create symlink (force overwrite if exists)
